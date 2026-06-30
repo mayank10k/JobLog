1135237278bb7cdbfa4c6c1381677a6a4d44ccf9
Mayank Mehta <mayankmehta433@gmail.com>
2026-06-30 19:50:02 +0530

    redis is included for sessions

--- backend/middleware/authmiddleware.js
+++ backend/middleware/authmiddleware.js
@@ -1,20 +1,31 @@
 const jwt=require('jsonwebtoken');
+const redisClient=require('./../config/redis')
 
-const jwtAuthMiddleware=(req,res,next)=>{
+const jwtAuthMiddleware=async (req,res,next)=>{
 
     const authorization=req.headers.authorization;
-    if(!authorization)return res.status(401).json({error:'token not found'});
+    // if(!authorization)return res.status(401).json({error:'token not found'});
+    if (!authorization || !authorization.startsWith("Bearer ")) {
+        return res.status(401).json({ error: "Authorization header missing or invalid" });
+    }
 
-    const token=req.headers.authorization.split(' ')[1];
+
+    const token=authorization.split(' ')[1];
     if(!token)return res.status(401).json({error:'Unauthorized'})
 
     try{
         const decoded=jwt.verify(token,process.env.JWT_SECRET)
         
+        //check if sessioin exist in redis
+        const storedToken=await redisClient.get(`session:${decoded.id}`);
+        if (!storedToken || storedToken !== token) {
+            return res.status(401).json({ message: "Session expired or invalid, please login again" });
+        }
+
         req.user=decoded;
         next();
     }catch(err){
-        
+        console.error(err);
         res.status(401).json({error:'Invalid token'})
     }
     
