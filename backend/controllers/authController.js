7bf030e8ab09a3cbe2a65e33ef895536f8ab5b01
Mayank Mehta <mayankmehta433@gmail.com>
2026-06-30 19:48:44 +0530

    redis is fixed in all the routes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,58 @@
+const redisClient = require('../config/redis');
+const User=require('./../models/User');
+const { generateToken } = require('../middleware/authmiddleware');
+
+const signup=async(req,res)=>{
+    try{
+        const data=req.body;
+
+        const newUser=new User(data);
+
+        const response=await newUser.save();
+        console.log("data saved");
+
+        const payload={
+            id:response.id
+        }
+
+        const token=generateToken(payload);
+        await redisClient.set(`session:${response.id}`,token,"EX",604800)
+
+        res.status(200).json({response:response,token:token})
+
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error:'internal serverr error'})
+    }
+}
+
+const login=async(req,res)=>{
+    try{
+        const {email,password}=req.body;
+        //find the username from db
+        const user=await User.findOne({email:email});
+
+        if(!user || !(await user.comparePassword(password))){
+        return res.status(401).json({error:"Invalid email or password"});
+        }
+
+        const payload={
+        id:user.id,
+        }
+        const token=generateToken(payload);
+
+        await redisClient.set(`session:${user.id}`,token,"EX",604800)
+
+        res.json({token});
+
+  }catch(err){
+    console.log(err);
+    res.status(500).json({error:'internal server error'})
+  }
+}
+
 const logout = async (req, res) => {
   try {
-    const redisClient = require("../config/redis");
-
     // delete session from Redis using user id
     await redisClient.del(`session:${req.user.id}`);
 
@@ -10,3 +61,5 @@ const logout = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+module.exports={signup,login,logout};
