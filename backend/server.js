df0207d87dec31869e46573ea268ccd6f26092e0
Mayank Mehta <mayankmehta433@gmail.com>
2026-07-07 16:39:46 +0530

    installed cors

--- backend/server.js
+++ backend/server.js
@@ -7,16 +7,27 @@ const startReminderCron = require("./reminderCron");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 // Start the cron job
 startReminderCron();
+
+const cors = require("cors");
+// app.use(cors());
+
+//for production
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 const userAuth=require('./routes/auth');
 const jobRoute=require('./routes/jobs')
 
 
-app.use('/user',userAuth);
-app.use('/job',jobRoute);
+app.use('/auth',userAuth);
+app.use('/jobs',jobRoute);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
